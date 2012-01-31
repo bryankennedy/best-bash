1ab7d3f977363e6df5897253df0ce1cc60f67a35
bryan kennedy <bkennedy@smm.org>
2012-01-31 11:32:08 -0600

    Better git aliases
    Adding a few git aliases for some other common tasks.

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -126,12 +126,12 @@ fi
 ###############################################################################
 # Git
 ###############################################################################
-alias gs='git status -s'
 # Removing the -b flag
 # It isn't supported until Git 1.7.2 which isn't common in CentOS and other
 # Linux rpms just yet. So, removing this makes this bash profile a bit more
 # portable for now.
 # alias gs='git status -sb'
+alias gs='git status -s'
 
 alias gc='git commit -v'
 alias ga='git add'
@@ -139,14 +139,17 @@ alias gap='git add -p'
 alias gaa='git add -A'
 alias gco='git checkout'
 alias gd='git diff'
+alias gdt='git difftool'
 alias gps='git push'
 alias gpsm='git push origin master'
 alias gpsd='git push origin develop'
 alias gpl='git pull'
 alias gplm='git pull origin master'
 alias gpld='git pull origin develop'
+alias gf='git fetch'
 alias gb='git branch'
-alias gba='git branch -a'
+alias gba='git branch -a -v -v'
+alias gun='git reset HEAD'             # Unstage added changes
 
 ###############################################################################
 # Reload bash
