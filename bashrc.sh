052bb01398ed01cb34acc9c29d8cba6b9a6d838e
bryan kennedy <bkennedy@smm.org>
2012-03-05 14:54:06 -0600

    Better fix for the sed bug.

--- bashrc.sh
+++ bashrc.sh
@@ -167,7 +167,7 @@ fi
 # TODO combine the two conditionals above
 
 # Autocomplete hostnames
-complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed s/,.*//g | uniq | grep -v "\["`;)" ssh
+complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e 's/,.*//g' | uniq | grep -v "\["`;)" ssh
 
 ############################################################
 # Security
