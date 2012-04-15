d5d643a04d02e6aeacb9cd9b549e83ae27cf131e
bryan kennedy <digital@mysteryexperience.com>
2012-04-15 12:19:16 -0500

    Adding the autojump cd utility
    I installed this with homebrew on one of my machines and it seems very
    useful. This is required as part of the install, but it's conditional
    based on whether you have it installed on your machine or not.

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -62,6 +62,11 @@ function mc() {
   mkdir -p "$*" && cd "$*" && pwd
 }
 
+# Autojump - https://github.com/joelthelion/autojump
+if [ -f `brew --prefix`/etc/autojump ]; then
+  . `brew --prefix`/etc/autojump
+fi
+
 ###############################################################################
 # Extracting
 ###############################################################################
