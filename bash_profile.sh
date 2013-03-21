88470590050e2f8e6b56c0c5637e38ee8098268f
bryan kennedy <digital@mysteryexperience.com>
2013-03-21 00:13:19 -0500

    Fixing bug with profile
    Now that we're creating an alias for the best-bash directory, this file needs to point there also.

--- bash_profile.sh
+++ bash_profile.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 ############################################################
 # bash_profile.sh
-# 
+#
 # This file is primarily for routing conditional situations.
 # This file is loaded first in "Interactive login" shells.
 # This includes:
 #   SSH or tty sessions on Linux
 #   Every new terminal window in OS X
 ############################################################
-HOMEDIR=$HOME/.dotfiles/best-bash
+HOMEDIR=$HOME/.best-bash
 
 # Include bashrc
 . $HOMEDIR/bashrc.sh
