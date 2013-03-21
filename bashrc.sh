d573bdc192b4b7ebc0846c3666ec49e6ada85df3
bryan kennedy <digital@mysteryexperience.com>
2013-03-20 22:56:03 -0500

    Removing obsolute PS1 directives
    These weren't doing anything on the OSX or Linux side of things

--- bashrc.sh
+++ bashrc.sh
@@ -71,29 +71,6 @@ export DATE=$(date +%Y-%m-%dT%H:%M:%S%z)
 ############################################################
 # Prompt
 ############################################################
-# set a fancy prompt (non-color, unless we know we "want" color)
-case "$TERM" in
-    xterm-color) color_prompt=yes;;
-esac
-
-if [ -n "$force_color_prompt" ]; then
-    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
-  # We have color support; assume it's compliant with Ecma-48
-  # (ISO/IEC-6429). (Lack of such support is extremely rare, and such
-  # a case would tend to support setf rather than setaf.)
-  color_prompt=yes
-    else
-  color_prompt=
-    fi
-fi
-
-if [ "$color_prompt" = yes ]; then
-    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
-else
-    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
-fi
-unset color_prompt force_color_prompt
-
 # If you edit the prompt at all, make sure to escape any
 # non-printing characters with:
 # \[033[ and \]
